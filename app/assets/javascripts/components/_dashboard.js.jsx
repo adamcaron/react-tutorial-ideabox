a6fe65dff202bd42b95a2fc772731feb3e10cde5
Adam Caron <adamwcaron@gmail.com>
2015-10-23 15:58:50 -0600

    Load all ideas with Ajax

--- app/assets/javascripts/components/_dashboard.js.jsx
+++ app/assets/javascripts/components/_dashboard.js.jsx
@@ -1,63 +1,74 @@
-var Dashboard = React.createClass({ // react component
-  getInitialState: function () {
-    return {value: 'what\'s up'}
+var Dashboard = React.createClass({
+  getInitialState: function() {
+    return {value: 'Come join us', rawIdeas: []}
   },
-  onButtonBoxClick: function () {
-    // update the state of value ^^^
-    this.setState({value: "You clicked a button, and here I am all of a sudden."})
+  onButtonBoxClick: function() {
+    this.setState({value: "You clicked a button, and here I am all of a sudden."});
   },
+  componentDidMount: function() {
+    $.ajax({
+      url: '/api/v1/ideas.json',
+      type: 'GET',
+      success: function(response) {
+        this.setState({rawIdeas: response});
+      }.bind(this)
+    });
+  },
+
   render: function() {
-     // the return statement can only return one single element. There can be nested elements, but only one top level
     return (
       <div className='dashboard'>
-        <h1>Preparation is Key!!!!</h1>
+        <h1>Preparation is key!!!!</h1>
 
         <ButtonBox onButtonClick={this.onButtonBoxClick} />
         <ContentBox text={this.state.value} />
-        <IdeaBox />
+
+        <IdeaBox ideas={this.state.rawIdeas} />
       </div>
     )
   }
 });
 
 var ButtonBox = React.createClass({
-  handleClick: function () {
-    this.props.onButtonClick();
+  handleClick: function() {
+    this.props.onButtonClick()
   },
-  render: function () {
+  render: function() {
     return (
       <div>
-        <button onClick={this.handleClick}>Click Me!</button>
+        <button onClick={this.handleClick} >
+          Click me!
+        </button>
       </div>
     )
   }
 });
 
 var ContentBox = React.createClass({
-  render: function () {
+  render: function() {
     return (
       <div className='content-box'>
         <h1>{this.props.text}</h1>
       </div>
     )
   }
-})
+});
 
 var IdeaBox = React.createClass({
-  getIdeasFromApi: function () {
-    $.ajax({
-      url: '/api/v1/ideas.json',
-      type: 'GET',
-      success: function (response) {
-        console.log(response);
-      }
-    })
-  },
-  render: function () {
-    this.getIdeasFromApi();
+  render: function() {
+    var ideaElements = this.props.ideas.map(function(idea, index) {
+      return (
+        <div className='idea' key={index} >
+          <h1>{idea.title}</h1>
+          <h3>{idea.body}</h3>
+          <p>Quality: {idea.quality}</p>
+        </div>
+      )
+    });
+
     return (
       <div>
-        <h1>We are in the idea box component.</h1>
+        {ideaElements}
       </div>
     )
   }
