74a0bdfbaded6e623c6a126136e090c6a95f4a2e
Adam Caron <adamwcaron@gmail.com>
2015-10-23 15:02:42 -0600

    Get initial state to render and update on button click

---
+++ app/assets/javascripts/components/_dashboard.js.jsx
@@ -0,0 +1,43 @@
+var Dashboard = React.createClass({ // react component
+  getInitialState: function () {
+    return {value: 'what\'s up'}
+  },
+  onButtonBoxClick: function () {
+    // update the state of value ^^^
+    this.setState({value: "You clicked a button, and here I am all of a sudden."})
+  },
+  render: function() {
+     // the return statement can only return one single element. There can be nested elements, but only one top level
+    return (
+      <div className='dashboard'>
+        <h1>Preparation is Key!!!!</h1>
+
+        <ButtonBox onButtonClick={this.onButtonBoxClick} />
+        <ContentBox text={this.state.value} />
+      </div>
+    )
+  }
+});
+
+var ButtonBox = React.createClass({
+  handleClick: function () {
+    this.props.onButtonClick();
+  },
+  render: function () {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me!</button>
+      </div>
+    )
+  }
+});
+
+var ContentBox = React.createClass({
+  render: function () {
+    return (
+      <div className='content-box'>
+        <h1>{this.props.text}</h1>
+      </div>
+    )
+  }
+})
